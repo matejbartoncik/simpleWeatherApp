81983f8327d8506d92155ad09a5ddd7bd1d0221c
Matej Bartoncik <matejbartoncik@seznam.cz>
2024-01-06 23:57:28 +0100

    Pridana funkce pro zápis / tvorbu dat

--- src/main/java/fei/upce/cz/Main.java
+++ src/main/java/fei/upce/cz/Main.java
@@ -1,17 +1,30 @@
 package fei.upce.cz;
 
 
-
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner user_input = new Scanner(System.in);
-        System.out.println("Zadejte lokaci kde chcete zjistit počasí:");
-        String pocasko = user_input.nextLine();
-        WeatherApp pocasi = new WeatherApp(pocasko);
-        pocasi.APICallCords();
-        pocasi.APICallWeather();
+
+        do {
+            System.out.print("Zadejte lokaci kde chcete zjistit počasí: ");
+            String userLocation = user_input.nextLine();
+            WeatherApp pocasi = new WeatherApp(userLocation);
+            System.out.print("Chcete zjistit počasí v jiné lokaci? (ano/ne)");
+            String odpoved = user_input.nextLine();
+            if (odpoved.equals("ne")) {
+                System.out.println("Ukončuji program...");
+                System.out.println("Program ukončen");
+                break;
+            }
+            if (!odpoved.equals("ano")) {
+                System.out.println("Neplatná odpoveď");
+            }
+        } while (true);
     }
+
+
 }
+
--- src/main/java/fei/upce/cz/WeatherApp.java
+++ src/main/java/fei/upce/cz/WeatherApp.java
@@ -7,6 +7,10 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -16,59 +20,75 @@ import java.util.Date;
 
 public class WeatherApp {
     private String user_location;
-    private double lat;
-    private double lon;
-    private String jsonResponse;
-    private String weatherResponse;
 
     public WeatherApp(String user_location) {
         this.user_location = user_location;
         nameConvert();
     }
 
-    private void parseJsonResponse() {
+    private void parseJsonResponse(String jsonResponse) {   //Ziská JSON data z API  které se volá ve funkci APICallCords a předává je funkci APICallWeather
         JSONArray temp = new JSONArray(jsonResponse);
-        lat = temp.getJSONObject(0).getDouble("lat");
-        lon = temp.getJSONObject(0).getDouble("lon");
+        double lat = temp.getJSONObject(0).getDouble("lat");
+        double lon = temp.getJSONObject(0).getDouble("lon");
+        APICallWeather(lat, lon);
     }
 
-    private void parseWeatherResponse() {
+    private void parseWeatherResponse(String weatherResponse, String user_location) { //Ziská JSON data z API  které se volá ve funkci APICallWeather a vypíše je na konzoli;
         SimpleDateFormat formatter = new SimpleDateFormat(" HH:mm:ss");
         String timestamp = formatter.format(new Date());
-        Date date = new Date();
         JSONObject wrapObj = new JSONObject(weatherResponse);
         String description = wrapObj.getJSONArray("weather").getJSONObject(0).getString("description");
-        double temperature = round(wrapObj.getJSONObject("main").getDouble("temp"),1) ;
-        System.out.println(temperature + "---" + description + "--" + timestamp);
+        double temperature = round(wrapObj.getJSONObject("main").getDouble("temp"), 1);
+        String output = "\n"+"Lokace: " + user_location + "\n" + "Teplota: " + temperature + "\n" + "Popis: " + description + "\n" + "Čas: " + timestamp;
+        System.out.println(output);
+        dataOutput(output);
     }
-    private static double round (double value, int precision) {
+
+    private void dataOutput(String output_data) { //Zapisuje data do souboru dataPocasi.txt
+        File file = new File("dataPocasi.txt");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+            if (!file.exists()){
+                file.createNewFile();
+            }
+            System.out.println("Zapisuji data do souboru dataPocasi.txt");
+            writer.write("\n"+output_data);
+
+        } catch (IOException e) {
+            System.out.println("Chyba při zápisu do souboru");
+        }
+
+    }
+
+
+    private static double round(double value, int precision) { //Zaokrouhluje čísla
         int scale = (int) Math.pow(10, precision);
         return (double) Math.round(value * scale) / scale;
     }
 
-    private void nameConvert() {
+    private void nameConvert() { //Převádí název lokace na formát který je použitelný v URL
         user_location = user_location.replace(" ", ".");
         user_location = user_location.toLowerCase();
+        APICallCords(user_location);
     }
 
 
-    public void APICallWeather() {
+    public void APICallWeather(double lat, double lon) { //Volá API a získává data o počasí z funkce ApiCallCords
         final String APIKEY = "7fcce5dabf80a14dd91a0dc3ec527e3b";
         final String url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + APIKEY + "&units=metric&lang=cz";
         final String newUrl = "https://api.openweathermap.org/data/3.0/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + APIKEY + "&units=metric&lang=cz";
 
 
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            try {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) { //Vytváří HTTP klienta
+            try { //Získává data z API
                 HttpGet request = new HttpGet(url);
                 try (CloseableHttpResponse response = httpClient.execute(request)) {
-                    weatherResponse = EntityUtils.toString(response.getEntity());
-                    parseWeatherResponse();
+                    String weatherResponse = EntityUtils.toString(response.getEntity());
+                    parseWeatherResponse(weatherResponse,user_location);
                 }
             } catch (Exception e) {
                 throw new RuntimeException(e);
             } finally {
-                try {
+                try { //Zavírá HTTP klienta
                     httpClient.close();
                 } catch (IOException e) {
                     throw new RuntimeException(e);
@@ -79,7 +99,7 @@ public class WeatherApp {
         }
     }
 
-    public void APICallCords() {
+    public void APICallCords(String user_location) { //Volá API a získává souřadnice lokace které předává funkci APICallWeather
         final String APIKEY = "7fcce5dabf80a14dd91a0dc3ec527e3b";
         final String url = "https://api.openweathermap.org/geo/1.0/direct?q=" + URLEncoder.encode(user_location, StandardCharsets.UTF_8) + "&limit=1&appid=" + APIKEY;
 
@@ -88,8 +108,8 @@ public class WeatherApp {
             try {
                 HttpGet request = new HttpGet(url);
                 try (CloseableHttpResponse response = httpClient.execute(request)) {
-                    jsonResponse = EntityUtils.toString(response.getEntity());
-                    parseJsonResponse();
+                    String jsonResponse = EntityUtils.toString(response.getEntity());
+                    parseJsonResponse(jsonResponse);
                 }
             } catch (IOException e) {
                 System.out.println("Něco se pokazilo");
